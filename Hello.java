a444cf447834c6ee6ed99d3bc296cbeab247d9d9
sherin muhter <sherinmuhter@gmail.com>
2019-06-13 21:44:55 -0700

    added Hello world program

---
+++ Hello.java
@@ -0,0 +1,23 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO AGAIN AND AGAIN");
+	}
+
+}
+
+
+
+////This is a Java file
+//public class HelloWorld {
+//	private int unusedInt = 0;
+//	private boolean unusedBoolean = true;
+//
+//	public static void main(String[] args) {
+//		System.out.println(sayHello());
+//	}
+//
+//	private static String sayHello() {
+//		return "Hello World";
+//	}
+//}
